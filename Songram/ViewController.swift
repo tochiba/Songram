384ba54b5925961d974e846e4689f8ae299cb069
tochiba <c104kn@gmail.com>
2016-04-30 00:04:17 +0900

    first commit

---
+++ Songram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Songram
+//
+//  Created by 千葉 俊輝 on 2016/04/30.
+//  Copyright © 2016年 Toshiki Chiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
